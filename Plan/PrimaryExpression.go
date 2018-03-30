32829688ce64a1828acf5a51339750144ebb6986
xitongsys <xitongsys@gmail.com>
2018-03-30 10:31:59 +0800

    ..

---
+++ Plan/PrimaryExpression.go
@@ -0,0 +1,12 @@
+package Plan
+
+import (
+	"github.com/antlr/antlr4/runtime/Go/antlr"
+	"github.com/xitongsys/guery/DataSoruce"
+)
+
+type PrimaryExpressionNode struct {
+}
+
+type NullLiteralNode struct {
+}
