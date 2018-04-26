b84381c56cfa75702cbda98b387016a956fb19cb
xitongsys <xitongsys@gmail.com>
2018-04-26 09:39:22 +0800

    ..

--- Master/Master.go
+++ Master/Master.go
@@ -1,4 +1,33 @@
 package Master
 
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/golang/protobuf/proto"
+	"github.com/xitongsys/guery/pb"
+)
+
 type Master struct {
+	Topology  *Topology
+	StartTime time.Time
+}
+
+func NewMaster() *Master {
+	m := &Master{
+		Topology:  NewTopology(),
+		StartTime: time.Now(),
+	}
+	return m
+}
+
+func (self *Master) SendHeartbeat(stream pb.GueryMaster_SendHeartbeatServer) error {
+	var location *pb.Location
+	for {
+		heartbeat, err := stream.Recv()
+		if err == nil {
+		} else {
+		}
+	}
 }
--- Master/Topology.go
+++ Master/Topology.go
@@ -13,6 +13,18 @@ type ExecutorInfo struct {
 	LastHeartBeatTime time.Time
 }
 
+func NewExecutorInfo(hb *pb.Heartbeat) *ExecutorInfo {
+	return &ExecutorInfo{
+		Name:              hb.Location.ExecutorName,
+		Heartbeat:         *hb,
+		LastHeartBeatTime: time.Now(),
+	}
+
+}
+
+//////////////////////////////
+
+//rack////////////////////////
 type Rack struct {
 	sync.RWMutex
 	Name             string
@@ -21,51 +33,11 @@ type Rack struct {
 	TotalExecutorNum int32
 }
 
-type DataCenter struct {
-	sync.RWMutex
-	Name             string
-	Racks            map[string]*Rack
-	IdleExecutorNum  int32
-	TotalExecutorNum int32
-}
-
-type Topology struct {
-	sync.RWMutex
-	DataCenters      map[string]*DataCenter
-	IdleExecutorNum  int32
-	TotalExecutorNum int32
-}
-
-func NewTopology() *Topology {
-	return &Topology{
-		DataCenters: make(map[string]*DataCenter),
-	}
-}
-
-func NewDataCenter(name string) *DataCenter {
-	return &DataCenter{
-		Name:  name,
-		Racks: make(map[string]*Rack),
-	}
-}
-
 func NewRack(name string) *Rack {
 	return &Rack{
 		Name:      name,
 		Executors: make(map[string]*Rack),
 	}
-}
-
-func (self *Topology) GetDataCenter(name string) *DataCenter {
-	self.RLock()
-	defer self.RUnlock()
-	return self.DataCenters[name]
-}
-
-func (self *DataCenter) GetRack(name string) *Rack {
-	self.RLock()
-	defer self.RUnlock()
-	return self.Racks[name]
 }
 
 func (self *Rack) GetExecutor(name string) *ExecutorInfo {
@@ -85,3 +57,83 @@ func (self *Rack) DropExecutor(name string) {
 	defer self.Unlock()
 	delete(self.Executor, name)
 }
+
+//////////////////////////////
+
+//DataCenter/////////////////
+type DataCenter struct {
+	sync.RWMutex
+	Name             string
+	Racks            map[string]*Rack
+	IdleExecutorNum  int32
+	TotalExecutorNum int32
+}
+
+func NewDataCenter(name string) *DataCenter {
+	return &DataCenter{
+		Name:  name,
+		Racks: make(map[string]*Rack),
+	}
+}
+
+func (self *DataCenter) GetRack(name string) *Rack {
+	self.RLock()
+	defer self.RUnlock()
+	return self.Racks[name]
+}
+
+func (self *DataCenter) AddRack(rack *Rack) {
+	self.Lock()
+	self.Unlock()
+	self.Racks[rack.Name] = rack
+}
+
+///////////////////////////
+
+//Topology/////////////////
+type Topology struct {
+	sync.RWMutex
+	DataCenters      map[string]*DataCenter
+	IdleExecutorNum  int32
+	TotalExecutorNum int32
+}
+
+func NewTopology() *Topology {
+	return &Topology{
+		DataCenters: make(map[string]*DataCenter),
+	}
+}
+
+func (self *Topology) GetDataCenter(name string) *DataCenter {
+	self.RLock()
+	defer self.RUnlock()
+	return self.DataCenters[name]
+}
+
+func (self *Topology) AddDataCenter(dc *DataCenter) {
+	self.Lock()
+	defer self.Unlock()
+	self.DataCenters[dc.Name] = dc
+}
+
+func (self *Topology) UpdateExecutorInfo(hb *pb.Heartbeat) {
+	dc := self.GetDataCenter(hb.Location.DataCenter)
+	if dc == nil {
+		dc = NewDataCenter(hb.Location.DataCenter)
+		hb.AddDataCenter(dc)
+	}
+
+	rack := dc.GetRack(hb.Location.Rack)
+	if rack == nil {
+		rack = NewRack(hb.Location.Rack)
+	}
+
+	if exeInfo := rack.GetExecutor(hb.Location.ExecutorName); exeInfo == nil {
+		exeInfo = NewExecutorInfo(hb)
+
+	} else {
+
+	}
+}
+
+///////////////////////////
