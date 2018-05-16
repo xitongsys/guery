f79f0d112f79b11fc1558a4b8953b30f61a482e2
xitongsys <xitongsys@gmail.com>
2018-05-16 11:26:26 +0800

    ..

--- Util/Func.go
+++ Util/Func.go
@@ -5,32 +5,6 @@ import (
 	"reflect"
 )
 
-func Val2Float(val interface{}) float64 {
-	var res float64
-	switch val.(type) {
-	case int64:
-		res = float64(val.(int64))
-	case float64:
-		res = val.(float64)
-	case string:
-		fmt.Sscanf(val.(string), "%f", &res)
-	}
-	return res
-}
-
-func Val2Int(val interface{}) int64 {
-	var res int64
-	switch val.(type) {
-	case int64:
-		res = val.(int64)
-	case float64:
-		res = int64(val.(float64))
-	case string:
-		fmt.Sscanf(val.(string), "%d", &res)
-	}
-	return res
-}
-
 func Arithmetic(leftVal interface{}, rightVal interface{}, op Operator) interface{} {
 	var res interface{}
 	lT, rT := reflect.TypeOf(leftVal).Kind(), reflect.TypeOf(rightVal).Kind()
