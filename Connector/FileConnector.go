b0c1538c7d00dcbbb1d19300847f6299e185af9a
xitongsys <xitongsys@gmail.com>
2018-05-22 10:21:26 +0800

    adding file connector

--- Connector/FileConnector.go
+++ Connector/FileConnector.go
@@ -12,21 +12,17 @@ import (
 	"github.com/xitongsys/guery/Util"
 )
 
-type LocationFileConnector struct {
+type FileConnector struct {
 	Metadata  *Util.Metadata
 	FileList  []string
 	FileIndex int
 	FileType  string
 }
 
-func GetFiles(filePattern string) ([]string, error) {
-	return filepath.Glob(filePattern)
-}
-
-func NewLocalFileConnector(schema, table string) (*TestConnector, error) {
+func NewFileConnector(schema, table string) (*TestConnector, error) {
 	var err error
-	res := &LocationFileConnector{}
-	conf := Config.Conf["LOCALFILE"]["METASTORE"][schema][table]
+	res := &FileConnector{}
+	conf := Config.Conf["FILE"]["METASTORE"][schema][table]
 	res.FileType = conf["TYPE"]
 	res.FileList, err = GetFiles(conf["DATA"])
 	if err != nil {
@@ -42,17 +38,17 @@ func NewLocalFileConnector(schema, table string) (*TestConnector, error) {
 	return res
 }
 
-func (self *TestConnector) GetMetadata() *Util.Metadata {
+func (self *FileConnector) GetMetadata() *Util.Metadata {
 	return self.Metadata
 }
 
-func (self *TestConnector) ReadRow() (*Util.Row, error) {
+func (self *FileConnector) ReadRow() (*Util.Row, error) {
 
 }
 
-func (self *TestConnector) SkipRows(num int64) {
+func (self *FileConnector) SkipRows(num int64) {
 	self.Index += num
 }
 
-func (self *TestConnector) ReadRowByColumns(colIndexes []int) (*Util.Row, error) {
+func (self *FileConnector) ReadRowByColumns(colIndexes []int) (*Util.Row, error) {
 }
