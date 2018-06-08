c3d084e2c79353ae0be7314ab2a4b200c6aaaede
xitongsys <xitongsys@gmail.com>
2018-06-08 15:09:48 +0800

    fix ui bug

--- Master/UI/js/info_task.js
+++ Master/UI/js/info_task.js
@@ -18,35 +18,36 @@ function InfoTasksToTable(infos) {
     var content='';
     infos.sort(function(a,b){return b.TaskId - a.TaskId;});
     for(var i=0; i<infos.length; i++){
-	Tasks[infos[i].TaskId] = infos[i]
+	Tasks[infos[i].TaskId] = infos[i];
 	
 	rec='<tr>';
 	if(infos[i].Status=="DONE"){
-	    rec='<tr class="active">'
+	    rec='<tr class="success">';
 	}else if (infos[i].Status=="DOING"){
-	    rec='<tr class="success">'
+	    rec='<tr class="success">';
 	}else if (infos[i].Status=="TODO") {
-	    rec='<tr class="info">'
+	    rec='<tr class="info">';
 	}else if (infos[i].Status=="FAILED"){
-	    rec='<tr class="danger">'
+	    rec='<tr class="danger">';
 	}
 
 	rec=rec + '<td style="width:25%;">';
 	
-	rec=rec + '<ul class="list-group" style="font-size:8pt;">';
-	rec=rec + '<li class="list-group-item list-group-item-info">ID: ' + infos[i].TaskId + '</li>';
-	rec=rec + '<li class="list-group-item list-group-item-info">Status: ' + infos[i].Status + '</li>';
-	rec=rec + '<li class="list-group-item list-group-item-info">Priority: ' + infos[i].Priority + '</li>';
-	rec=rec + '<li class="list-group-item list-group-item-info">Begin: ' + infos[i].BeginTime + '</li>';
-	rec=rec + '<li class="list-group-item list-group-item-info">End: ' + infos[i].EndTime + '</li>';
-	rec=rec + '<li class="list-group-item list-group-item-info">Commit: ' + infos[i].CommitTime + '</li>';
+	rec=rec + '<ul class="list-group" style="font-size:8pt; margin:0px; padding:0px;">';
+	rec=rec + '<li class="list-group-item list-group-item-info" style="margin:0px; padding:3px;">ID: ' + infos[i].TaskId + '</li>';
+	rec=rec + '<li class="list-group-item list-group-item-info" style="margin:0px; padding:3px">Status: ' + infos[i].Status + '</li>';
+	rec=rec + '<li class="list-group-item list-group-item-info" style="margin:0px; padding:3px">Priority: ' + infos[i].Priority + '</li>';
+	rec=rec + '<li class="list-group-item list-group-item-info" style="margin:0px; padding:3px">Begin: ' + infos[i].BeginTime + '</li>';
+	rec=rec + '<li class="list-group-item list-group-item-info" style="margin:0px; padding:3px">End: ' + infos[i].EndTime + '</li>';
+	rec=rec + '<li class="list-group-item list-group-item-info" style="margin:0px; padding:3px">Commit: ' + infos[i].CommitTime + '</li>';
+	rec=rec + '<li class="list-group-item list-group-item-info" style="margin:0px; padding:3px">Error: ' + infos[i].ErrInfo + '</li>';	
 	rec=rec + "</ul>";
 
 	rec=rec + '<div align="right">';
 	rec=rec + '<div class="btn-group" role="group">';
-	rec = rec + '<button type="button" class="btn btn-info" onclick=\'ShowDetail("' + infos[i].TaskId + '")\'>Details</button>';
+	rec = rec + '<button type="button" class="btn btn-info btn-xs" onclick=\'ShowDetail("' + infos[i].TaskId + '")\'>Details</button>';
 	if(infos[i].Status=="DOING" || infos[i].Status=="TODO"){
-	    rec = rec + '<button type="button" class="btn btn-danger" onclick=\'CancelTask("' + infos[i].TaskId + '")\'>Cancel</button>' 
+	    rec = rec + '<button type="button" class="btn btn-danger btn-xs" onclick=\'CancelTask("' + infos[i].TaskId + '")\'>Cancel</button>' 
 	}
 	rec=rec + "</div>";
 	rec=rec + "</div>";
@@ -56,8 +57,9 @@ function InfoTasksToTable(infos) {
 
 
 	//query
-	var progressBar =  '<div class="progress">';
-	rec=rec + '<td>' + '<div class="alert alert-info">' + infos[i].Query + "</div>";
+	rec=rec + '<td>';
+	
+	var progressBar =  '<div class="progress" style="margin:0px;">';
 	if(infos[i].Status=="FAILED"){
 	    progressBar = progressBar + '<div class="progress-bar progress-bar-danger progress-bar-striped" role="progressbar" aria-valuenow="' + infos[i].Progress + '" aria-valuemin="0" aria-valuemax="100" style="width: ' + infos[i].Progress + '%;">'
 	}else if (infos[i].Status=="DOING"){
@@ -66,9 +68,9 @@ function InfoTasksToTable(infos) {
 	    progressBar = progressBar + '<div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow="' + infos[i].Progress + '" aria-valuemin="0" aria-valuemax="100" style="width: ' + infos[i].Progress + '%;">'	    
 	}
         progressBar = progressBar + infos[i].Progress + '%' + '</div></div>';
-
-	
 	rec = rec + progressBar;
+
+	rec=rec + '<div class="alert alert-info" style="margin:0px;">' + infos[i].Query + "</div>";
 	rec = rec + '</td>';
 	
 	rec=rec+'</tr>';
