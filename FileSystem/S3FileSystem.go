031f6695d1f25d10979f78a9495533cc1d4ce341
xitongsys <xitongsys@gmail.com>
2018-06-21 10:26:43 +0800

    del log

--- FileSystem/S3FileSystem.go
+++ FileSystem/S3FileSystem.go
@@ -3,7 +3,6 @@ package FileSystem
 import (
 	"fmt"
 	"io"
-	"log"
 	"math/rand"
 	"os"
 	"regexp"
@@ -76,7 +75,6 @@ func (self *S3FileSystem) List(fl *FileLocation) (fileLocations []*FileLocation,
 	var S3Conf = &aws.Config{
 		Region: aws.String(Config.Conf.Runtime.S3Region),
 	}
-	log.Println("======", Config.Conf.Runtime.S3Region)
 	svc := s3.New(session.New(), S3Conf)
 	prefix, bucket, key, err := SplitS3URL(fl.Location)
 	if err != nil {
